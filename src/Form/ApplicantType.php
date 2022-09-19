2552034a74b83219fac744babcecaf5e2a568e51
Rimantas <rimantas.narusevicius@gmail.com>
2022-09-19 20:16:18 +0300

    created form

---
+++ src/Form/ApplicantType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Applicant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ApplicantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('first_name')
+            ->add('last_name')
+            ->add('address')
+            ->add('email')
+            ->add('phone_number')
+            ->add('unique_house_number')
+            ->add('project_number')
+            ->add('compensation_received')
+            ->add('created')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Applicant::class,
+        ]);
+    }
+}
